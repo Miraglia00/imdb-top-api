3b840f7ccd11e7f9b4edba94a6a41d1e95a4509d
Gömböcz Zsolt <zsolt.gombocz00@gmail.com>
2022-02-09 21:15:47 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,17 @@
+
+const express = require("express");
+
+const app = express();
+
+require('dotenv').config();
+
+
+app.get('/', (req, res) => {
+    res.json({message: "IMDb guesser backend. It will allow to scrape some information from IMDb to use it in front-end."});
+});
+
+app.get('/:id', (req, res) => {
+    res.json(req.params.id);
+});
+
+app.listen(4444, () => console.log('[SERVER]: Listening on port 4444....'));
