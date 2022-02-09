28193dbee256f1289aa0f905b86c4f0ae364399c
Gömböcz Zsolt <zsolt.gombocz00@gmail.com>
2022-02-10 00:07:14 +0100

    Added DB include

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const app = express();
 
 require('dotenv').config();
 
+require('./database/db');
+
 
 app.get('/', (req, res) => {
     res.json({message: "IMDb guesser backend. It will allow to scrape some information from IMDb to use it in front-end."});
