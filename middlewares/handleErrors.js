045efc1ad4e7c78a5c2af62e8d5b434dbee03279
Gömböcz Zsolt <zsolt.gombocz00@gmail.com>
2022-03-17 13:16:03 +0100

    error handler handles schema validated object and strings and error objs

--- middlewares/handleErrors.js
+++ middlewares/handleErrors.js
@@ -1,9 +1,19 @@
 module.exports = (err,req,res,next) => {
     const statusCode = res.statusCode !== 200 ? res.statusCode : 500;
+
     res.status(statusCode);
-    res.json({
-        status: statusCode,
-        message: err.message,
-        stack: process.env.NODE_ENV === 'production' ? '😎' : err.stack
-    });
+
+    if(typeof err === 'string' || (typeof err === 'object' && err.message !== undefined && err.stack !== undefined)) {
+        res.json({
+            status: statusCode,
+            message: err.message,
+            stack: process.env.NODE_ENV === 'production' ? '😎' : err.stack
+        });
+    }else{
+        res.json({
+            status: statusCode,
+            error: err,
+        });
+    }
+    
 };
