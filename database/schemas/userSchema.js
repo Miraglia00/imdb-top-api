c139037277bde14c7c81651f63844579229537ef
Gömböcz Zsolt <zsolt.gombocz00@gmail.com>
2022-03-17 11:30:54 +0100

    removed scores property and fixed string length check

--- database/schemas/userSchema.js
+++ database/schemas/userSchema.js
@@ -4,17 +4,13 @@ const {Schema} = mongoose;
 const userSchema = new Schema({
     username: {
         type: String, 
-        min: [5, 'Minimum character for the username must be at least 5!'], 
-        min: [15, 'Maximum character for the username is 15!'], 
+        minlength: [5, 'Minimum character for the username must be at least 5!'], 
+        maxlength: [15, 'Maximum character for the username is 15!'], 
         required: [true, 'This field is required! (username)']
     },
     password: {
         type: String, 
         required: [true, 'This field is required! (password)']
-    },
-    scores: {
-        type: Array,
-        default: []
     }
 }, {strict: true});
 
