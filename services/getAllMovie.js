725f225e16b4fc32d010122aaaeb73f516493ad1
Gömböcz Zsolt <zsolt.gombocz00@gmail.com>
2022-03-17 16:27:58 +0100

    Fixed limit, now gives back random when limit is 1

--- services/getAllMovie.js
+++ services/getAllMovie.js
@@ -4,9 +4,15 @@ const movieModel = db.model('movies', schema);
 
 async function getAllMovie(limit=null) {
     limit = (typeof limit === 'number') ? limit : null;
-    let data = await movieModel.find({}).limit(limit);
-
-    return data;
+    let returnData = [];
+    if(limit > 1 || limit === null) {
+        returnData = await movieModel.find({}).limit(limit);
+    }else if(limit === 1){
+        returnData = await movieModel.find(null);
+        returnData = returnData[Math.floor(Math.random() * 250)];
+    }
+    
+    return returnData;
 }
 
 module.exports = {
--- services/getAllTVShows.js
+++ services/getAllTVShows.js
@@ -4,9 +4,15 @@ const tvShowsModel = db.model('tvshows', schema);
 
 async function getAllTVShows(limit=null) {
     limit = (typeof limit === 'number') ? limit : null;
-    let data = await tvShowsModel.find({}).limit(limit);
-
-    return data;
+    let returnData = [];
+    if(limit > 1 || limit === null) {
+        returnData = await tvShowsModel.find({}).limit(limit);
+    }else if(limit === 1){
+        returnData = await tvShowsModel.find(null);
+        returnData = returnData[Math.floor(Math.random() * 250)];
+    }
+    
+    return returnData;
 }
 
 module.exports = {
