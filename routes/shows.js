d24634a0431b8f9565640dba33bdf0dda6243280
Gömböcz Zsolt <zsolt.gombocz00@gmail.com>
2022-03-17 14:54:27 +0100

    added schema skeleton to show up in swagger

--- routes/shows.js
+++ routes/shows.js
@@ -1,3 +1,57 @@
+/**
+ * @swagger
+ * components:
+ *  schemas:
+ *   Show:
+ *      type: object
+ *      required:
+ *       - movie_id
+ *      properties:
+ *       movie_id:
+ *        type: integer
+ *        description: IMDb id of the show.
+ *       titles:
+ *        type: orderedMap
+ *        description: Holds the titles of the show in different langueges.
+ *       short_plot:
+ *        type: string
+ *        description: Short plot of the movie.
+ *       genre:
+ *        type: string
+ *        description: String of the genre of the show.
+ *       origin:
+ *        type: string
+ *        description: Country of origin, places where the show recorded.
+ *       released_year:
+ *        type: string
+ *        description: First appearance date.
+ *       length:
+ *        type: string
+ *        description: Length of the movie or a episode.
+ *       directors:
+ *        type: string
+ *        description: Directors of the show.
+ *       stars:
+ *        type: string
+ *        description: Start or voice actors in the show.
+ *       imdb_score:
+ *        type: string
+ *        desciption: Official score of the show from IMDb.
+ *      example:
+ *       movie_id: tt0111161
+ *       titles:
+ *        hu-HU: A remény rabjai
+ *        en-US: The Shawshank Redemption
+ *       short_plot: Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.
+ *       genre: drama
+ *       origin: United States
+ *       released_year: 1994
+ *       length: 142 min
+ *       directors: Frank Darabont
+ *       stars: Tim Robbins, Morgan Freeman, Bob Gunton
+ *       imdb_score: 9.3       
+ */
+
 const express = require('express');
 const router = express.Router();
 const {getAllMovie} = require('../services/getAllMovie');
