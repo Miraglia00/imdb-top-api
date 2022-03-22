869573126f837663807f0c93ec56f6f3b957c1aa
Gömböcz Zsolt <zsolt.gombocz00@gmail.com>
2022-03-22 15:04:08 +0100

    Added token check function before entering route

--- routes/user.js
+++ routes/user.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
 
-router.post('/register', userController.registerUser);
+router.post('/register', userController.checkToken, userController.registerUser);
+
+router.post('/login', userController.checkToken, userController.loginUser);
 
 module.exports = router;
