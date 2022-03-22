9a58eecf0c1e277a5a6f5c205639c6ca842e6ca8
Gömböcz Zsolt <zsolt.gombocz00@gmail.com>
2022-03-22 15:04:35 +0100

    included method in errors

--- middlewares/handleErrors.js
+++ middlewares/handleErrors.js
@@ -1,17 +1,18 @@
 module.exports = (err,req,res,next) => {
     const statusCode = res.statusCode !== 200 ? res.statusCode : 500;
-
     res.status(statusCode);
 
     if(typeof err === 'string' || (typeof err === 'object' && err.message !== undefined && err.stack !== undefined)) {
         res.json({
             status: statusCode,
+            method: res.req.method,
             message: err.message,
             stack: process.env.NODE_ENV === 'production' ? '😎' : err.stack
         });
     }else{
         res.json({
             status: statusCode,
+            method: res.req.method,
             error: err,
         });
     }
