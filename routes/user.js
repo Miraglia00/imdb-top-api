3390bb0ea49a31f10a40becf4d804fbaaab05cc6
Gömböcz Zsolt <zsolt.gombocz00@gmail.com>
2022-04-04 11:05:37 +0200

    added new route /checktoken to call from front end to check logjn token

--- routes/user.js
+++ routes/user.js
@@ -6,4 +6,7 @@ router.post('/register', userController.checkToken, userController.registerUser)
 
 router.post('/login', userController.checkToken, userController.loginUser);
 
+router.post('/checktoken', userController.checkIfTokenValid);
+
+
 module.exports = router;
