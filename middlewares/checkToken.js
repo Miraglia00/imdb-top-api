cb285af11131e20b5aa5ceb91b2d74c6163fc3b1
Gömböcz Zsolt <zsolt.gombocz00@gmail.com>
2022-04-04 11:05:05 +0200

    Token checking middleware refactored

--- middlewares/checkToken.js
+++ middlewares/checkToken.js
@@ -1,13 +1,18 @@
 const jwt = require('jsonwebtoken');
 
 module.exports = async (req,res,next) => {
-    if(req.headers['authorization'] !== undefined || req.headers['authorization'] !== null) {
-        const token = req.headers['authorization'].split('Bearer ')[1];
-        let verify = await jwt.verify(token, process.env.JWT_SECRET || 'strongsecret0011');
-        if(verify) {
-            res.locals.token = true;
-        }else{
+    if(req.headers['authorization'] !== undefined && req.headers['authorization'] !== null) {
+        const token = req.headers['authorization'].split('Bearer ');
+        if(token.length === 1) {
             res.locals.token = false;
+        }else{
+            let verify = await jwt.verify(token[1], process.env.JWT_SECRET || 'strongsecret0011');
+            if(verify) {
+                res.locals.token = true;
+                res.locals.user = verify;
+            }else{
+                res.locals.token = false;
+            }
         }
     }else{
         res.locals.token = false;
